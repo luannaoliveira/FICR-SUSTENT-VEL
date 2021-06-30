296973c39022af23ff20e560cf561a25917c5ba2
Luanna Oliveira <46655733+luannaoliveira@users.noreply.github.com>
2021-06-30 12:49:35 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav">
+   <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
+  </div>
+  <router-view/>
+</template>
+
+<style>
+* {
+  margin: 0;
+  font-family: 'Roboto', sans-serif;
+  -webkit-font-smoothing: antialiased;
+}
+</style>
