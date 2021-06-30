b6bd4f1c175b298201e8b8b620d026a22a130d7a
Luanna Oliveira <46655733+luannaoliveira@users.noreply.github.com>
2021-06-30 12:51:32 -0300

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// import Home from '../views/Home.vue'
+import Index from '../views/Index.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Index',
+    component: Index
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+  },
+  {  path: '/inicial',
+  name: 'inicial',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/inicial.vue')
+},
+{  path: '/cadastro',
+  name: 'Cadastro',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/Cadastro.vue')
+},
+{  path: '/perfil',
+  name: 'Perfil',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/Perfil.vue')
+},
+{  path: '/login',
+  name: 'login',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/login.vue')
+}
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
